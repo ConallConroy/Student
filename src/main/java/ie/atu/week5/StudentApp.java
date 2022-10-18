4be6a0cb25ea45b3ce72e11b71dd53d13bdeb5be
Conall Conroy <g00409207@atu.ie>
2022-10-18 13:34:48 +0100

    Prompt user for students name.

--- src/main/java/ie/atu/week5/StudentApp.java
+++ src/main/java/ie/atu/week5/StudentApp.java
@@ -1,4 +1,13 @@
 package ie.atu.week5;
 
+import java.util.Scanner;
+
 public class StudentApp {
+
+    public static void main(String[] args) {
+        System.out.println("Please enter student name: ");
+        Scanner input = new Scanner(System.in);
+        double StuName = input.nextDouble();
+        System.out.println("You entered: " + StuName);
+    }
 }
