5c6734f7d453643a39c061d2b9875ae09d9459ce
Conall Conroy <g00409207@atu.ie>
2022-10-18 13:58:18 +0100

    Added new file called Student

---
+++ src/main/java/ie/atu/week5/Student.java
@@ -0,0 +1,31 @@
+package ie.atu.week5;
+
+public class Student {
+    private String StuName;
+    private String StuEmail;
+    private String StuCourse;
+
+    public String getStuName() {
+        return StuName;
+    }
+
+    public void setStuName(String stuName) {
+        this.StuName = stuName;
+    }
+
+    public String getStuEmail() {
+        return StuEmail;
+    }
+
+    public void setStuEmail(String stuEmail) {
+        this.StuEmail = stuEmail;
+    }
+
+    public String getStuCourse() {
+        return StuCourse;
+    }
+
+    public void setStuCourse(String stuCourse) {
+       this. StuCourse = stuCourse;
+    }
+}
